5e0f08ac0b9632e921a2e220df788c8bee8e50c2
Luis Preciado <luisapreciado99@gmail.com>
2020-04-06 11:29:31 -0500

    SecurityGrapher

---
+++ src/components/SecurityGrapher/SecurityGrapher.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Chart from "react-apexcharts";
+import { formatAPIRequest } from "../../shared/functions";
+import StockCharForm from "../../components/StockChartForm/StockChartForm";
+
+export default class SecurityGrapher extends React.Component {
+  componentDidMount() {
+    this.onSubmitFormHandler = this.onSubmitFormHandler.bind(this)
+    this.onSubmitFormHandler()
+  }
+
+
+  state = {
+    ticker: "",
+    loading: true,
+    error: null,
+    data: [],
+    initialGraph: {
+      ticker : "EPZM",
+      timeFrame : "15",
+      startDate : "1572651390",
+      endDate : "1573910590",
+    }
+  };
+
+  onSubmitFormHandler(formValues = this.state.initialGraph) {
+    const data = formatAPIRequest(
+      formValues.ticker,
+      formValues.timeFrame,
+      formValues.startDate,
+      formValues.endDate
+    );
+    data
+      .then((data) => this.setState({ loading: false, data: data, ticker: formValues.ticker }))
+      .catch((error) => this.setState({ error: error }));
+  }
+
+  render() {
+    const options = {
+      chart: {
+        type: "candlestick",
+        height: 350,
+      },
+      title: {
+        text: this.state.ticker,
+        align: "left",
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true,
+        },
+      },
+    };
+
+    let customChart = null;
+    if (!this.state.loading) {
+      const final = [
+        {
+          data: this.state.data,
+        },
+      ];
+      customChart = (
+        <Chart
+          options={options}
+          series={final}
+          type="candlestick"
+          height={350}
+        />
+      );
+    }
+
+    return (
+      <div>
+        {customChart}
+        <StockCharForm forex={this.props.forex} formSubmit={this.onSubmitFormHandler} />
+      </div>
+    );
+  }
+}
--- src/containers/Forex/Forex.js
+++ src/containers/Forex/Forex.js
@@ -1,47 +1,10 @@
 import React from "react";
-import { getTickerExpectedReturns } from "../../shared/functions";
-import StockChartForm from "../../components/StockChartForm/StockChartForm";
+import SecurityGrapher from "../../components/SecurityGrapher/SecurityGrapher"
 
-export default class Stocks extends React.Component {
-  componentDidMount() {
-    this.onSubmitFormHandler = this.onSubmitFormHandler.bind(this);
-    this.onSubmitFormHandler();
-  }
-  onSubmitFormHandler(formValues = this.state.initialTicker) {
-    console.log(formValues)
-  }
-
-  state = {
-    loading: true,
-    ticker: "AAPL",
-    expectedReturns: 0,
-    stDev: 0,
-    timePeriod: "",
-    error: null,
-    initialTicker: {
-      ticker: "EPZM",
-      timeFrame: "15",
-      startDate: "1572651390",
-      endDate: "1573910590",
-    },
-  };
-
-  render() {
-    let stockInfo = null;
-    if (!this.state.loading) {
-      stockInfo = (
-        <div style={{margin: "auto"}}>
-          <h2>{this.state.ticker}</h2>
-          <p>Expected Returns: {this.state.expectedReturns} %</p>
-          <p>Risk: {this.state.stDev} %</p>
-        </div>
-      );
+export default class StockChart extends React.Component {
+    render(){
+        return(
+            <SecurityGrapher forex={true}/>
+        )
     }
-    return (
-      <div>
-        {stockInfo}
-        <StockChartForm forex={true}formSubmit={this.onSubmitFormHandler} />
-      </div>
-    );
-  }
 }
