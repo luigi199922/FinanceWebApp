254751c315764f135189572c4ae4d3d709603280
Luis Preciado <luisapreciado99@gmail.com>
2020-04-06 12:01:06 -0500

    rm key

--- src/shared/key.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "&token=boqata7rh5rfjhndqf1g"
-export const STOCK_URL = "https://finnhub.io/api/v1/quote?symbol:AAPL&token:" + API_KEY
-export const KEY = "boqata7rh5rfjhndqf1g"
