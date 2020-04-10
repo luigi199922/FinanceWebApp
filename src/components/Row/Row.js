c953fc62ec564db12698ed337ccb242ffcd47623
Luis Preciado <luisapreciado99@gmail.com>
2020-04-10 11:14:03 -0500

    inTheMoney Row bgcolor

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,8 +1,11 @@
 import React from "react";
 import TD from "./TableData/TableData";
+import classes from './Row.module.css'
+
 
 const Row = ({ option, optionDisplay }) => {
   let TableData = null;
+  const {inTheMoney} = option;
   const properties = Object.keys(optionDisplay);
   TableData = properties.map((val, key) => {
     if (val === "lastTradeDateTime") {
@@ -12,6 +15,6 @@ const Row = ({ option, optionDisplay }) => {
     }
     return <TD>{option[val]}</TD>;
   });
-  return <tr>{TableData}</tr>;
+  return <tr className={inTheMoney === "TRUE" ? classes.Row : null}>{TableData}</tr>;
 };
 export default Row;
