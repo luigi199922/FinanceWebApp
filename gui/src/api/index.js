d94ca0a48d341e68a801bf669f5a82ce2afec73a
Luis Preciado <luisapreciado99@gmail.com>
2020-04-18 10:10:20 -0500

    gui economic added

--- gui/src/api/index.js
+++ gui/src/api/index.js
@@ -1,14 +1,17 @@
 import axios from "axios";
+import HashTable from "../shared/hashTable";
 const url = "https://covid19.mathdro.id/api";
+const econDataUrl =
+  "https://finnhub.io/api/v1/economic?code=MA-XXX-678073&token=boqata7rh5rfjhndqf1g";
 
 export const fetchData = async (country) => {
-    let flexUrl = url
+  let flexUrl = url;
 
-    if(country) {
-        flexUrl = url + "/countries/" + country
-    }
+  if (country) {
+    flexUrl = url + "/countries/" + country;
+  }
 
-    try {
+  try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
     } = await axios.get(flexUrl);
@@ -16,6 +19,44 @@ export const fetchData = async (country) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const formatUrl = (countryCode) => {
+  return econDataUrl.replace("XXX", countryCode);
+};
+
+export const fetchEconomicData = async (country) => {
+  try {
+    const { data } = await axios.get(formatUrl(country));
+    const modifiedData = [];
+    data.map((dailyData) => {
+      modifiedData.unshift({ date: dailyData[0], value: dailyData[1] });
+    });
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchDailyCountries = async () => {
+  const economicCodesUrl =
+    "https://finnhub.io/api/v1/economic/code?token=boqata7rh5rfjhndqf1g";
+  const { data } = await axios.get(economicCodesUrl);
+  const countries = new HashTable();
+  const economicCodes = new HashTable();
+  data.forEach(async (item) => {
+    console.log(item);
+    if (countries.getItem(item.country) === null) {
+      const code = item.code.slice(3, 6);
+      await countries.setItem(item.country, code);
+    }
+    // if (economicCodes.getItem(item.name) == null) {
+    //   const code = item.code.slice(7);
+    //   economicCodes.setItem(item.name, code);
+    // }
+  });
+
+  return countries;
 };
 
 export const fetchDailyData = async () => {
--- gui/src/containers/Economic/Economic.jsx
+++ gui/src/containers/Economic/Economic.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {fetchEconomicData, fetchDailyCountries} from '../../api/'
+import { Line } from 'react-chartjs-2'
 
 const Economic = () => {
+    const [ dailyData, setDailyData] = useState([])
+    const [ countryCode, setCountryCode] = useState("USA")
+    const [countries, setCountries] = useState()
+    useEffect(()=> {
+        const fetchAPIData = async() => {
+            setDailyData(await fetchEconomicData(countryCode))
+        }
+        fetchAPIData() 
+        // const getCountries = async() => {
+        //     setCountries(await fetchDailyCountries())
+        // }
+        // getCountries()
+        
+    },[])
+    if(countries){
+        console.log(countries)
+    }
+    const lineChart = (
+        dailyData.length ? (
+            <Line 
+            data={{
+                labels: dailyData.map(({date }) => date),
+                datasets: [{
+                    data: dailyData.map((({value}) => value)),
+                    label: 'Value',
+                    borderColor: '#3333ff',
+                    fill: true,
+                }]
+            }}
+        />
+        ) : 
+        null
+        
+    )
+    
     return(
         <div>
-            Economic
+            Economic Data : USA ADP EMPLOYMENT CHANGE
+            {lineChart}
         </div>
     )
 }
